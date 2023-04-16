42b1456f21e101cf70bf77140f492bf0ec83143d
Hitendra Quessou <sanjay_quessou@yahoo.co.uk>
2023-04-16 22:47:37 +0100

    installed axios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import generateJoke from './generate.Joke';
+import './styles/main.scss';
+import laughing from './assets/laughing.svg';
+
+const laughImg = document.getElementById('laughImg');
+laughImg.src = laughing;
 
 console.log(generateJoke());
